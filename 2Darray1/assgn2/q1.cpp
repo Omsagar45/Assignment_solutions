a9fe6cbfbda6d408f6d8014fb1aeb83e2d65a0ac
Omsagar45 <119053209+Omsagar45@users.noreply.github.com>
2023-10-17 10:01:52 +0530

    Add files via upload

--- 2Darray1/assgn2/q1.cpp
+++ 2Darray1/assgn2/q1.cpp
@@ -1,1 +1,36 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int m;
+    int n;
+    cout<<"Enter rows of matrix: ";
+    cin>>m;
+    cout<<"Enter columns of matrix: ";
+    cin>>n;
+    int a[m][n];
 
+    cout<<"enter matrix: "<<endl;
+    for(int i=0;i<m;i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+           cin>>a[i][j];
+        }
+        
+    }
+
+    cout<<"Output: "<<endl;
+    for(int i=0;i<m;i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+           if(i==j || i+j==n-1)
+           {
+                cout<<a[i][j]<<" ";
+           }
+           else cout<<" ";
+        }
+        cout<<endl;
+    }
+}
